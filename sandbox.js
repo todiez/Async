961ded2f36a80f7def6ca0826c8d18ef0f52b3b1
tybo-dev <tobias@diez.guru>
2022-12-26 17:16:02 +0100

    error log added

--- sandbox.js
+++ sandbox.js
@@ -8,9 +8,12 @@ request.addEventListener('readystatechange', () => {
     console.log(request, request.readyState);
     //once the state is 4 we can do something with the response text
 
-    if (request.readyState === 4) {
+    if (request.readyState === 4 && request.status === 200) {
         console.log(request.responseText)
+    } else if (request.readyState === 4) {
+        console.log("could not fetch data");
     }
+
 });
 
 
