2800e59e94a5754cde45813c00061927a90bdfea
tybo-dev <tobias@diez.guru>
2022-12-29 20:11:48 +0100

    promises chained

--- sandbox.js
+++ sandbox.js
@@ -1,28 +1,26 @@
-const getTodos = (url, callbackFunction) => {
-
+const getTodos = (url) => {
   //create Promise:
-  return new Promise(() => {
-  // create request object, it is built in JS
-  const request = new XMLHttpRequest();
+  return new Promise((resolve, reject) => {
+    // create request object, it is built in JS
+    const request = new XMLHttpRequest();
 
-  //fires every time the state changes in the request
-  request.addEventListener("readystatechange", () => {
-    //console.log(request, request.readyState);
-    //once the state is 4 we can do something with the response text
-    if (request.readyState === 4 && request.status === 200) {
-      //console.log(request.responseText);
-      const data = JSON.parse(request.responseText); //takes the String of JSONfile and converts it to objects
-      callbackFunction(undefined, data);
-    } else if (request.readyState === 4) {
-      //console.log("could not fetch data");
-      callbackFunction('could not fetch data', undefined);
-    }
-  });
-  //open is to set up the request, not the the request itself
-  //open takes two arguments, first is a string and kind of, second is the endpoint
-  request.open("GET", url);
-  request.send();
-
+    //fires every time the state changes in the request
+    request.addEventListener("readystatechange", () => {
+      //console.log(request, request.readyState);
+      //once the state is 4 we can do something with the response text
+      if (request.readyState === 4 && request.status === 200) {
+        //console.log(request.responseText);
+        const data = JSON.parse(request.responseText); //takes the String of JSONfile and converts it to objects
+        resolve(data);
+      } else if (request.readyState === 4) {
+        //console.log("could not fetch data");
+        reject('error getting data');
+      }
+    });
+    //open is to set up the request, not the the request itself
+    //open takes two arguments, first is a string and kind of, second is the endpoint
+    request.open("GET", url);
+    request.send();
   });
 };
 
@@ -40,28 +38,48 @@ console.log(2);
 //     });
 // });
 
-//fake promise, no real request made, just as an example
-const getSomething = () => {
-  return new Promise((resolve, reject) => {
-    resolve('something new');
-    reject('error and rejected');
-  });
-};
+// //fake promise, no real request made, just as an example
+// const getSomething = () => {
+//   return new Promise((resolve, reject) => {
+//     resolve("something new");
+//     reject("error and rejected");
+//   });
+// };
 
-//.then can be added to a promises
-getSomething().then((data) => {
-  console.log(data);
-}, (err) => {
-  console.log(err);
+// //.then can be added to a promises
+// getSomething().then(
+//   (data) => {
+//     console.log(data);
+//   },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
+
+// //more elegant solution with catch:
+// getSomething()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+
+getTodos('todos/todos.json').then(data => {
+  console.log('promise resolved:' , data)
+  //return gives another promise:
+  return getTodos('todos/todos2.json');
+}).then(data => {
+  console.log('promise 2 resolved:' , data)
+  //return gives another promise:
+  return getTodos('todos/todos3.json');
+}).then(data => {
+  console.log('promise 3 resolved:' , data)
+ 
+}).catch(err => {
+  console.log('promise rejected:' , err);
 });
-
-//more elegant solution with catch:
-getSomething().then(data => {
-  console.log(data);
-}).catch(err =>{
-  console.log(err)
-})
-
 
 console.log(3);
 console.log(4);
