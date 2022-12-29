ff409da191f6f3e6ec2b2de492362f33ea2e25ff
tybo-dev <tobias@diez.guru>
2022-12-29 20:29:10 +0100

    fetch then chained

--- fetchApi.js
+++ fetchApi.js
@@ -1,18 +1,21 @@
-
 console.log(1);
 console.log(2);
 
-
-
 //Fetch returns a promise, therefore we can use .then
-fetch('todos/todos.json').then((response) => {
+fetch("todos/todos.json")
+  .then((response) => {
     //then === resolove
-    console.log("fetch resolved", response);
-}).catch((err) => {
+
+    //.json() is a method provided by fetch
+    return response.json(); //another promise which takes some time to fulfill, use return to take another .then
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
     //catch === reject
     console.log("fetch error", err);
-});
-
+  });
 
 console.log(3);
 console.log(4);
