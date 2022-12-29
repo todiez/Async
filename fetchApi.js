854ee6feb98f3b8de89a635ddedbd15ef240d24d
tybo-dev <tobias@diez.guru>
2022-12-29 21:16:23 +0100

    throwing an error

--- fetchApi.js
+++ fetchApi.js
@@ -5,20 +5,23 @@ console.log(2);
 
 //an async functions always returns a promise
 const getTodos = async () => {
-    //fetch return a promise, await stops from assigning to response till 
-    //the promise is resolved
-
-    console.log("async function started");
-    const response = await fetch('todos/todos.json');
-    console.log(response);
-    const data = await response.json();
-    console.log("inside getToDos:" , data);
-    return data;
+  //fetch return a promise, await stops from assigning to response till
+  //the promise is resolved
+  console.log("async function started");
+  const response = await fetch("todos/todofs.json");
+  if (response.status !== 200) {
+    console.log("Error fetching data");
+    throw new Error("Cannot fetch data"); //creating a new Error Object
+    //if an error inside an async function is thrown, the promise gets rejected
+  }
+  const data = await response.json();
+  return data;
 };
-
-
-const test = getTodos();
-console.log(test);
+console.log(3);
+//.then is once necessary
+getTodos()
+  .then((data) => console.log("resolved: ", data))
+  .catch((err) => console.log("error while fetching: ", err));
 
 //Fetch returns a promise, therefore we can use .then
 // fetch("todos/todos.json")
@@ -35,5 +38,4 @@ console.log(test);
 //     console.log("fetch error", err);
 //   });
 
-console.log(3);
 console.log(4);
